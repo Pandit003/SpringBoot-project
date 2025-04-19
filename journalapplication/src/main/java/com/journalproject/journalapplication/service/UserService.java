f448afb3f334635a190599bc18991a88b49005b0
Pandit.P <pardoom.pandit@inventrax.in>
2025-04-19 15:44:02 +0530

    commit for sonar cloud

--- journalapplication/src/main/java/com/journalproject/journalapplication/service/UserService.java
+++ journalapplication/src/main/java/com/journalproject/journalapplication/service/UserService.java
@@ -3,7 +3,10 @@ package com.journalproject.journalapplication.service;
 import java.util.Arrays;
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,15 +15,28 @@ import org.springframework.stereotype.Component;
 import com.journalproject.journalapplication.entity.User;
 import com.journalproject.journalapplication.repository.UserRepository;
 @Component
+@Slf4j
 public class UserService {
     @Autowired
     private UserRepository UserRepository;
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
+//    private static final Logger loger = LoggerFactory.getLogger(UserService.class);
+//    @Slf4j using this notation no need to initialize this object
+
+
     public void saveNewUser(User user){
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
-        UserRepository.save(user);
+        try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("USER"));
+            UserRepository.save(user);
+        }catch (Exception e){
+//            loger.error("Error occured "+e);  // use when logger initialized
+            log.error("Error occured "+e);
+            log.warn("Error occured "+e);
+            log.info("Error occured "+e);
+            log.debug("Error occured "+e);
+        }
     }
     public void saveAdmin(User user){
         user.setPassword(passwordEncoder.encode(user.getPassword()));
